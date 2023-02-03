86c1c0bfcb5d3000cecfdc6a6ff7e97b783ed4b6
F15999 <marcos.inca@fravega.com.ar>
2023-02-03 09:13:10 -0300

    Add loop

--- src/main.py
+++ src/main.py
@@ -7,17 +7,28 @@ PAGE_URL = "https://icanhas.cheezburger.com/"
 MEMES_FOLDER = "memes"
 
 
-def _get_memes_from_page():
-    page = requests.get(PAGE_URL)
+def _get_memes_from_page(count):
+    urls = get_url_memes_list(PAGE_URL)
+
+    while len(urls) <= count:
+        urls += get_url_memes_list(PAGE_URL)
+
+    _saving_memes_in_folder(urls, count)
+
+
+def get_url_memes_list(page_url):
+    page = requests.get(page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     image_tags = soup.find_all('img')
     urls = list(filter(lambda k: 'data-src' in str(k) and '/full/' in str(k), image_tags))
     urls = [img['data-src'] for img in urls]
+    return urls
 
-    count = 0
-    for i, url in enumerate(urls):
+
+def _saving_memes_in_folder(urls, count):
+    for i, url in enumerate(urls[:count]):
         response = requests.get(url)
-        with open(f"{MEMES_FOLDER}/images{i+1}.jpg", "wb+") as f:
+        with open(f"{MEMES_FOLDER}/images{i + 1}.jpg", "wb+") as f:
             f.write(response.content)
             count += 1
     print("Download complete")
@@ -25,5 +36,6 @@ def _get_memes_from_page():
 
 if __name__ == '__main__':
     os.mkdir(MEMES_FOLDER)
+    meme_amount = input("How many memes would you like to download?\n")
     print("Searching URLs")
-    _get_memes_from_page()
+    _get_memes_from_page(int(meme_amount))
